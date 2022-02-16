6be3c5db009fd2aa5c2ddeae5bc10fc3d8b794ab
greenislet <git.greenislet@gmail.com>
2022-02-16 15:40:05 +0100

    proper lang change

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -19,9 +19,7 @@ export default class Dropdown extends Controller {
   selectClick(event) {
     this.submitTarget.textContent = event.path[0].textContent;
     this.submitTarget.setAttribute("selected_idx", event.path[0].attributes["idx"].value);
-  }
 
-  submitClick(event) {
     var lang = Dropdown.locales[parseInt(this.submitTarget.attributes["selected_idx"].value)];
     window.location = window.location.protocol + "//" + window.location.host + window.location.pathname + `?lang=${lang}`;
   }
